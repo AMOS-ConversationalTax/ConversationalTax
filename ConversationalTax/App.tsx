e476aeb63737a7264505f86f25f75f0466203ba0
Gabriel Bauer <gabweb@gabweb.de>
2018-04-18 20:43:40 +0200

    Added TSLint

--- ConversationalTax/App.tsx
+++ ConversationalTax/App.tsx
@@ -1,7 +1,7 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
  */
@@ -11,12 +11,13 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-interface Props {};
-export default class App extends Component<Props> {
-  render() {
+interface IProps {}
+
+export default class App extends Component<IProps> {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
