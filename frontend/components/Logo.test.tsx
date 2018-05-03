84a9877790f661832cd5c1567e3b40f51039a047
Gabriel Bauer <gabweb@gabweb.de>
2018-05-03 20:50:25 +0200

    feat: Added frontend unit tests

--- frontend/components/Logo.test.tsx
+++ frontend/components/Logo.test.tsx
@@ -1,12 +1,15 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Logo from './Logo';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+describe('Logo', () => {
+  it('renders correctly', () => {
+    renderer.create(
+      <Logo />
+    );
+  });
 });
+
