ffec41812f2891580eb2d57d0302573be733520d
Alexander Mahler <alex.domi.mahler@googlemail.com>
2018-05-08 21:43:07 +0200

    Reverted example scematics.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
   controllers: [AppController],
   components: [],
 })
