92bd5ceff6edb0c52f3b7da5af5b6bb0256e4d17
Dominik Probst <dpdominikprobst@gmail.com>
2018-05-13 02:08:07 +0200

    fix: Added a bitArrayToBase64String function to the base64 fix file

--- frontend/services/FileServices.tsx
+++ frontend/services/FileServices.tsx
@@ -105,83 +105,39 @@ export default class FileServices {
     // Convert a bit array into a base64 string
     public bitArrayToBase64String(inputBitArray: Array<number>): String {
 
-        // Base64 Coding
-        let base64Coding = {
-            "000000": "A",
-            "000001": "B",
-            "000010": "C",
-            "000011": "D",
-            "000100": "E",
-            "000101": "F",
-            "000110": "G",
-            "000111": "H",
-            "001000": "I",
-            "001001": "J",
-            "001010": "K",
-            "001011": "L",
-            "001100": "M",
-            "001101": "N",
-            "001110": "O",
-            "001111": "P",
-            "010000": "Q",
-            "010001": "R",
-            "010010": "S",
-            "010011": "T",
-            "010100": "U",
-            "010101": "V",
-            "010110": "W",
-            "010111": "X",
-            "011000": "Y",
-            "011001": "Z",
-            "011010": "a",
-            "011011": "b",
-            "011100": "c",
-            "011101": "d",
-            "011110": "e",
-            "011111": "f",
-            "100000": "g",
-            "100001": "h",
-            "100010": "i",
-            "100011": "j",
-            "100100": "k",
-            "100101": "l",
-            "100110": "m",
-            "100111": "n",
-            "101000": "o",
-            "101001": "p",
-            "101010": "q",
-            "101011": "r",
-            "101100": "s",
-            "101101": "t",
-            "101110": "u",
-            "101111": "v",
-            "110000": "w",
-            "110001": "x",
-            "110010": "y",
-            "110011": "z",
-            "110100": "0",
-            "110101": "1",
-            "110110": "2",
-            "110111": "3",
-            "111000": "4",
-            "111001": "5",
-            "111010": "6",
-            "111011": "7",
-            "111100": "8",
-            "111101": "9",
-            "111110": "+",
-            "111111": "/"
-        }
+        // Copied Input array - we might need to push some new elements later on
+        let inputBitArrayCopy: Array<number> = inputBitArray.slice();
 
-        // Iterate through the bit array (for bash64 we need steps of 6)
-        for(let i = 0; i < inputBitArray.length; i + 6) {
+        // Base64 Coding
+        let base64Coding: Array<string> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("");
+        
+        // Initialize the output string
+        let outputString: String = "";
+        
+        // Iterate through the bit array (for bash64 we need steps of six bits)
+        for(let i: number = 0; i < inputBitArray.length; i = i + 6) {
 
             // For the case there are not enough bits left
-            //if(i + 6 <=  )
+            if(i + 6 > inputBitArray.length) {
 
-             // TODO
+                // Add the additional bits
+                for(let y: number = i; y < inputBitArray.length; y++) {
+
+                    inputBitArrayCopy.push(0);
+
+                }
+
+            }
+
+            // Compute the corresponding decimal number to the six bits
+            let sixBitNumber: number = inputBitArrayCopy[i] * Math.pow(2, 5) + inputBitArrayCopy[i+1] * Math.pow(2, 4) + inputBitArrayCopy[i+2] * Math.pow(2, 3)
+                                     + inputBitArrayCopy[i+3] * Math.pow(2, 2) + inputBitArrayCopy[i+4] * Math.pow(2, 1) + inputBitArrayCopy[i+5] * Math.pow(2, 0);
+
+            outputString = outputString.concat(base64Coding[sixBitNumber]);
 
         }
+
+        return outputString;
 
     }
 
