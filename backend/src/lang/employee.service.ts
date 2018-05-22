133cc5e2e4165a56efbe89096728e4f9eab189e0
Alexander Mahler <alex.domi.mahler@googlemail.com>
2018-05-22 22:43:40 +0200

    feat: Creating a new contract with an exact starting date which is limited to the current date

--- backend/src/lang/employee.service.ts
+++ backend/src/lang/employee.service.ts
@@ -22,16 +22,32 @@ export default class EmployeeService {
         {
             this.setContractName(responseParam);
         }
+        if (responseAction === 'VertragMitDatum')
+        {
+            this.createContractWithDate(responseParam);
+        }
     }
 
     /**
-     * Set the Name inside of an existing Contract - Uses right now a Hardcoded ID.
+     * Set the Name inside of an existing Contract - Uses right now a hardcoded ID!!
      * @param {object} responseParameter - All parameters extracted from the origin response
      */
     public setContractName(responseParameter: object) {
         if (responseParameter[this.structValueString] != null){
             const contractName = responseParameter[this.structValueString][this.fieldsString][this.nameString][this.stringValueString];
             this.employmentContractService.editName('5b046b1d9bc44048d059d12f', contractName);
+        }
+    }
+
+    /**
+     * Creates a new contract with an exact starting date - Uses right now a hardcoded name!!
+     * @param {object} responseParameter - All parameters extracted from the origin response
+     */
+    public createContractWithDate(responseParameter: object) {
+        if (responseParameter[this.stringValueString] != null){
+            const contractDate = responseParameter[this.stringValueString];
+            const idPromise = this.employmentContractService.create('dummyGirl');
+            idPromise.then((id) => this.employmentContractService.editStartDateExact(id , contractDate));
         }
     }
 }
