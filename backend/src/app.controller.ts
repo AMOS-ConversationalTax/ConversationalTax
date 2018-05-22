5534e383032e8613eaf70f9b09f28c4694dee7db
EduardPotwigin <eduard.potwigin@gmail.com>
2018-05-22 08:23:39 +0200

    feat: added Rest Api Skeleton according to client side. plus testing

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,24 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Post, Put, Delete, Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   root(): string {
     return 'Hello World!';
+  }
+
+  @Post()
+  create(): string {
+    return 'Post arrived!';
+  }
+
+  @Put()
+  update(): string {
+    return 'Put arrived!';
+  }
+
+  @Delete()
+  delete() : string {
+    return 'Delete arrived!';
   }
 }
