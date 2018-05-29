c5848ed719aeb6ed39bc76487911f52e0a116279
Gabriel Bauer <gabweb@gabweb.de>
2018-05-29 18:25:35 +0200

    feat: created a seperate module for lang

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './database/user/user.module';
-import { ReminderModule } from './database/reminder/reminder.module';
 import { EmploymentContractModule } from './database/employmentContract/employmentContract.module';
 import { AppController } from './app.controller';
-import { LangController } from './lang/lang.controller';
-import { DialogFlowService } from './lang/dialog-flow.service';
+import { LangModule } from 'lang/lang.module';
 
 @Module({
-  imports: [UserModule, ReminderModule, EmploymentContractModule],
-  controllers: [AppController, LangController],
-  providers: [DialogFlowService],
+  imports: [EmploymentContractModule, LangModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
