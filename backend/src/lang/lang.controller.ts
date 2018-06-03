207dd2fd883b06d69c4cd7e210df3d0451140e61
Dominik Probst <dpdominikprobst@gmail.com>
2018-06-03 11:46:39 +0200

    fix: Avoid a race condition with the user entities

--- backend/src/lang/lang.controller.ts
+++ backend/src/lang/lang.controller.ts
@@ -89,7 +89,7 @@ export class LangController {
    * @param params URL params which contain the userID as well as the platform.
    * @returns {Promise<DetectIntentResponse[]>} - A promise containing the response
    */
-  private processAudiofile(file: any, params: AudioIntentParams): Promise<DetectIntentResponse[]> {
+  private async processAudiofile(file: any, params: AudioIntentParams): Promise<DetectIntentResponse[]> {
     if (file === undefined || file.buffer === undefined) {
       throw new BadRequestException('No audio file was uploaded');
     }
@@ -107,6 +107,6 @@ export class LangController {
       throw new BadRequestException('Unkown platform');
     }
 
-    return this.dialogFlowService.detectAudioIntent(encoding, sampleRate, base64Audio, params.u_id);
+    return await this.dialogFlowService.detectAudioIntent(encoding, sampleRate, base64Audio, params.u_id);
   }
 }
