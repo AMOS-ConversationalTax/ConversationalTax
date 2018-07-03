052e8c40a8682190d672af94c137143694e8e65e
Alexander Mahler <alex.domi.mahler@googlemail.com>
2018-07-03 20:30:10 +0200

    added missing jsdoc

--- backend/src/lang/intents/handlers/handler.abstract.ts
+++ backend/src/lang/intents/handlers/handler.abstract.ts
@@ -1,5 +1,4 @@
 /* tslint:disable:completed-docs */
-// TODO Alex has to add the JSDoc
 
 /**
  * Class to handle a specific Intent
@@ -17,8 +16,14 @@ export abstract class IntentHandler{
      */
     public async abstract handle(intentData: IIntentData): Promise<ReturnText | undefined>;
 
-    // TODO return type definition
-    protected extractData(data: object, parameterName: string, parameterType: string) {
+    /**
+     * Used to extract custom and normal entities from the parameter response of Dialogflow
+     * @param {object} data Json of all parameter given by the Dialogflow response
+     * @param {string} parameterName The name of the parameter which should be extracted
+     * @param {string} parameterType The type of the parameter which should be extracted
+     * @returns {string} The extracted parameter
+     */
+    protected extractData(data: object, parameterName: string, parameterType: string): string {
         const reducedParameter = this.reducer(data, this.getParameterPath(parameterName));
         if (reducedParameter !== null) {
             return reducedParameter;
@@ -26,16 +31,32 @@ export abstract class IntentHandler{
         return this.reducer(data, this.getCustomParameterPath(parameterName, parameterType));
     }
 
-    private getParameterPath(parameterName: string): string[] {
+    /**
+     * Creates a path for normal entity parameter
+     * @param {string} parameterName The name of the searched parameter
+     * @returns {Array<sting>} The path to the searched parameter
+     */
+    private getParameterPath(parameterName: string): Array<string> {
         return [this.FIELDS, parameterName, this.STRINGVALUE];
     }
 
+    /**
+     * Create a path for a custom entity parameter
+     * @param {string} parameterName The name of the searched parameter
+     * @param {string} parameterType The type of the searched parameter
+     * @returns {Array<sting>} The path to the searched parameter
+     */
     private getCustomParameterPath(parameterName: string, parameterType: string): string[] {
         return [this.FIELDS, parameterName, this.STRUCTVALUE, this.FIELDS, parameterType, this.STRINGVALUE];
     }
 
-    // TODO return type definition
-    private reducer(source: object, parameterPath: Array<string>) {
+    /**
+     * Reduces a given path to the searched parameter
+     * @param {object} source Json of all parameter given by the Dialogflow response
+     * @param {Array<string>} parameterPath The path to the needed parameter
+     * @returns {string} The parameter at the end of the given path
+     */
+    private reducer(source: object, parameterPath: Array<string>): string {
         return parameterPath.reduce((current: object, nextProp: string) => {
             if (typeof current === 'object' && current[nextProp] !== undefined) {
                 return current[nextProp];
